cfb27417aa9d2d88798011b4d588c3492e94d981
Matthew Southworth <mdsouthworth1@gmail.com>
2022-11-21 10:23:48 -0500

    1

--- assets/js/showbox.js
+++ assets/js/showbox.js
@@ -11,34 +11,5 @@
         $("#toggle").text("Show More");
         $("#text").slideUp();
       }
-    });
-  
-
-   $("#toggle2").click(function(){
-     var elem = $("#toggle2").text();
-     if (elem == "Show More") {
-       //Stuff to do when btn is in the see more state
-       $("#toggle2").text("Show Less");
-       $("#text2").slideDown();
-     } else {
-       //Stuff to do when btn is in the see less state
-       $("#toggle2").text("Show More");
-       $("#text2").slideUp();
-     }
-   });
- 
- 
-
-     $("#toggle3").click(function(){
-       var elem = $("#toggle3").text();
-       if (elem == "Show More") {
-         //Stuff to do when btn is in the see more state
-         $("#toggle3").text("Show Less");
-         $("#text3").slideDown();
-       } else {
-         //Stuff to do when btn is in the see less state
-         $("#toggle3").text("Show More");
-         $("#text3").slideUp();
-       }
-     });
+    })
     })
