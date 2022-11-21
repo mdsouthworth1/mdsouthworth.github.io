80682789b7e67ecc86feeaaad2e846b40e061ec1
Matthew Southworth <mdsouthworth1@gmail.com>
2022-11-21 09:57:49 -0500

    js

---
+++ assets/js/showbox.js
@@ -0,0 +1,45 @@
+
+   $(document).ready(function() {
+    $("#toggle").click(function(){
+      var elem = $("#toggle").text();
+      if (elem == "Show More") {
+        //Stuff to do when btn is in the see more state
+        $("#toggle").text("Show Less");
+        $("#text").slideDown();
+      } else {
+        //Stuff to do when btn is in the see less state
+        $("#toggle").text("Show More");
+        $("#text").slideUp();
+      }
+    });
+  });
+  
+$(document).ready(function() {
+   $("#toggle2").click(function(){
+     var elem = $("#toggle2").text();
+     if (elem == "Show More") {
+       //Stuff to do when btn is in the see more state
+       $("#toggle2").text("Show Less");
+       $("#text2").slideDown();
+     } else {
+       //Stuff to do when btn is in the see less state
+       $("#toggle2").text("Show More");
+       $("#text2").slideUp();
+     }
+   });
+ });
+ 
+  $(document).ready(function() {
+     $("#toggle3").click(function(){
+       var elem = $("#toggle3").text();
+       if (elem == "Show More") {
+         //Stuff to do when btn is in the see more state
+         $("#toggle3").text("Show Less");
+         $("#text23").slideDown();
+       } else {
+         //Stuff to do when btn is in the see less state
+         $("#toggle3").text("Show More");
+         $("#text3").slideUp();
+       }
+     });
+   });
